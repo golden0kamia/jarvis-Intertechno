2592ff50c150b70cdd13674c3dbd841f7905f0ce
Bastien Piguet <golden0kamia@gmail.com>
2018-01-17 15:32:45 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -6,3 +6,86 @@
 # pg for PluGin
 # XX is a short code for your plugin, ex: ww for Weather Wunderground
 # You can use translations provided in the language folders functions.sh
+source ./gpio
+
+pin=$pg_it_pin
+delay=$time
+
+gpio mode $pin out
+    
+
+function pg_it_send()
+{
+    adresse=$1
+    unit=$2
+    status=$3
+    group=$4
+    
+    start_send
+    send_adresse $adresse
+    send_bit $group #Send if the the command is in a group
+    send_bit $status #Send the status if th light [on|off]
+    send_unit $unit
+    stop_send
+}
+
+function start_send()
+{
+    gpio write $pin 1
+    sleep $delay
+    gpio write $pin 0
+    sleep $((delay*10+delay/2))
+}
+
+function send_adresse()
+{
+    for i in {25..0}
+    do
+        send_bit $(((adresse >> i) & 1))
+        sleep $delay
+    done
+}
+
+function send_unit()
+{
+    for i in {3..0}
+    do
+        send_bit $(((unit >> i) & 1))
+        sleep $delay
+    done
+}
+
+function stop_send()
+{
+    gpio write $pin 1
+    sleep $delay
+    gpio write $pin 0
+    sleep $((delay * 40))
+}
+
+function send_bit()
+{
+    if [ "$1" = "1" ]
+    then
+        gpio write $pin 1
+        sleep $delay
+        gpio write $pin 0
+        sleep $((delay * 5))
+        gpio write $pin 1
+        sleep $delay
+        gpio write $pin 0
+        sleep $delay
+    elif [ "$1" = "0" ]
+        gpio write $pin 1
+        sleep $delay
+        gpio write $pin 0
+        sleep $delay
+        gpio write $pin 1
+        sleep $delay
+        gpio write $pin 0
+        sleep $((delay * 5))
+    else
+        jv_error "Wrong bit send"
+        jv_exit
+    fi
+}
