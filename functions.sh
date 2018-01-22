d8fb2ad0c701c39dd47b551e51161ab27d13ea5f
Bastien Piguet <golden0kamia@gmail.com>
2018-01-22 13:56:50 +0100

    Adapt for C file use
    adapt the files to use the C file with wiringpi librairy

--- functions.sh
+++ functions.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Creator Bastien Piguet @golden0kamia
-
-source gpio.sh
-
 file='settings.json' #Settings file
 
 pinIN=$(jq .setting.pinIN $file)
@@ -10,106 +7,15 @@ pinOUT=$(jq .setting.pinOUT $file)
 
 gpio mode $pinIN in
 gpio mode $pinOUT out
-    
 
 function pg_it_send()
 {
-    adresse=$(jq .device.$1.adresse $file)
-    unit=$(jq .device.$1.unit $file)
-    delay=$((jq .device.$1.delay $file))
+    adresse=$((jq .device.$1.adresse $file)
+    unit=$((jq .device.$1.unit $file)
+	group=$3
     status=$2
-    
-    if [ "$3" = "1" ]
-    then
-        for i in {1..4}
-        do
-            start_send
-            send_adresse $adresse
-            send_bit 0 #Send to disable group action
-            send_bit $status #Send the status of the light [on|off]
-            send_unit $unit
-            stop_send
-        done
-    elif [ "$3" = "0" ]
-        for i in {1..4}
-        do
-            start_send
-            send_adresse $adresse
-            send_bit 1 #Send to enable group action
-            send_bit $status #Send the status of the light [on|off]
-            send_unit 0 #unit note use in group
-            stop_send
-        done
-    else
-        jv_error "Wrong bit send"
-        jv_exit
-    fi
-    
-    if [ "$2" = "1" ]
-    then
-        pg_it_return "turn_on" $1
-    else
-        pg_it_return "turn_off" $1
-    fi
-    sleep 2
-}
-
-########## Sending data ##########
-function start_send()
-{
-    gpio write $pin 1
-    usleep $delay
-    gpio write $pin 0
-    usleep $((delay*10+delay/2))
-}
-
-function send_adresse()
-{
-    for i in {25..0}
-    do
-        send_bit $(((adresse >> i) & 1))
-    done
-}
-
-function send_unit()
-{
-    for i in {3..0}
-    do
-        send_bit $(((unit >> i) & 1))
-    done
-}
-
-function stop_send()
-{
-    gpio write $pin 1
-    usleep $delay
-    gpio write $pin 0
-    usleep $((delay * 40))
-}
-
-function send_bit()
-{
-    if [ "$1" = "1" ]
-    then
-        gpio write $pin 1
-        usleep $delay
-        gpio write $pin 0
-        usleep $((delay * 5))
-        gpio write $pin 1
-        usleep $delay
-        gpio write $pin 0
-        usleep $delay
-    elif [ "$1" = "0" ]
-        gpio write $pin 1
-        usleep $delay
-        gpio write $pin 0
-        usleep $delay
-        gpio write $pin 1
-        usleep $delay
-        gpio write $pin 0
-        usleep $((delay * 5))
-    else
-        jv_error "Wrong bit send"
-        jv_exit
-    fi
+    delay=$((jq .device.$1.delay $file))
+	
+	./send433 $adresse $status $group $unit $delay
+	
 }
---
+++ transform.h
@@ -0,0 +1,52 @@
+#include <math.h>
+
+int ctoi(char *argv[])
+{
+	unsigned long int out = 0;
+    int length = sizeof(argv);
+    
+    for(int i = 0;i < length; i++)
+    {
+        if(argv[i] == '1')
+        {
+            out = out + (1*pow(10, length-1-i));
+        }
+        else if(argv[i] == '2')
+        {
+            out = out + (2*pow(10, length-1-i));
+        }
+        else if(argv[i] == '3')
+        {
+            out = out + (3*pow(10, length-1-i));
+        }
+        else if(argv[i] == '4')
+        {
+            out = out + (4*pow(10, length-1-i));
+        }
+        else if(argv[i] == '5')
+        {
+            out = out + (5*pow(10, length-1-i));
+        }
+        else if(argv[i] == '6')
+        {
+            out = out + (6*pow(10, length-1-i));
+        }
+        else if(argv[i] == '7')
+        {
+            out = out + (7*pow(10, length-1-i));
+        }
+        else if(argv[i] == '8')
+        {
+            out = out + (8*pow(10, length-1-i));
+        }
+        else if(argv[i] == '9')
+        {
+            out = out + (9*pow(10, length-1-i));
+        }
+        else if(argv[i] == '0')
+        {
+            out = out + (0*pow(10, length-1-i));
+        }
+    }
+    return out;
+}
