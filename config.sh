10f5b0065c6484a92b7f5dcccfac0c0d38178dfd
Bastien Piguet <golden0kamia@gmail.com>
2018-01-23 14:26:34 +0100

    delete usless files

--- config.sh
+++ config.sh
@@ -1,2 +1,2 @@
 #Configuration is in file settings.json
-#Check the wiki for information https://github.com/golden0kamia/jarvis-Intertechno/wiki
+#Check the wiki for more information https://github.com/golden0kamia/jarvis-Intertechno/wiki
